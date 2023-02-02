1f505cfdc27796f002ec6bdb35d259a63f478f75
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-02-02 22:45:39 +0300

    customize the theme pallete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import './App.css'
+import HeroSection from './components/pages/HeroSection'
 import Navbar from './components/pages/Navbar'
+import Services from './components/pages/Services'
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      background: {
+        paper: '#fff',
+      },
+      primary: {
+        main: '#173A5E',
+        secondary: '#46505A',
+      },
+    },
+    typography: {
+      fontFamily: 'Nunito',
+    },
+  })
   return (
     <div className='App'>
-      <Navbar />
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <HeroSection />
+        <Services />
+      </ThemeProvider>
     </div>
   )
 }
